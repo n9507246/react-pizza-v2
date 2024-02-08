aef746ae702c24150189d9bfc08d54f8d7d13557
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-08 11:59:07 +0500

    Создаём компонент, SCSS, import/export, props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/app.scss'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import PizzaBlock from'./components/PizzaBlock'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div class="wrapper">
+      <Header/>
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price='540'/>
+            <PizzaBlock title="Четыре сезона" price='310'/>
+            <PizzaBlock title="Сырная" price='450'/>
+            <PizzaBlock title="Перепони" price='390'/>
+            <PizzaBlock title="Чизбургер-пицца" price='740'/>
+            {/* <PizzaBlock title="Мексиканская" price='540'/> */}
+
+          </div>
+        </div>
+      </div>
     </div>
+  
+   </>
   );
 }
 
---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+export default function Component() {
+    return(
+        <>
+            <div class="categories">
+              <ul>
+                <li class="active">Все</li>
+                <li>Мясные</li>
+                <li>Вегетарианская</li>
+                <li>Гриль</li>
+                <li>Острые</li>
+                <li>Закрытые</li>
+              </ul>
+            </div>
+        </>
+    )
+}
