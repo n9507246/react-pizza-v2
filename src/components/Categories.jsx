5c5ba1aa66ca385145ced5879aee5991e0c15831
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-08 17:04:26 +0500

    рефакторинг

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import MySelect from "./UI/MySelect"
 
 export default function Caregories ({categoriesList}){  
@@ -8,39 +7,3 @@ export default function Caregories ({categoriesList}){
     </div>
   )
 }
-
-
-
-// function Category({name, isActive, selectCategory}){
-//   return( 
-//       <li 
-//         onClick={()=>{selectCategory(name)}} 
-//         className={isActive === true ? 'active' : ''}
-//       >
-//         {name}
-//       </li>
-//     )
-// }
-
-// export default function Caregories({categoriesList}) {
-  
-//   const [selectCategory, setSelectCategory] = useState(categoriesList[0].name)
-  
-//   return(
-//       <div class="categories">
-//         <ul>
-//           {
-//             categoriesList.map(
-//                 (dataCategory) => 
-//                   <Category 
-//                     name={dataCategory.name}
-//                     isActive={selectCategory === dataCategory.name }
-//                     selectCategory={setSelectCategory}
-//                     key={dataCategory.id}
-//                   />  
-//             )
-//           } 
-//         </ul>
-//       </div>
-//   )
-// }
