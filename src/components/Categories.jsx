c55e3665c304d1e01e1a615e7219c6fdcce64952
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-13 14:21:04 +0500

    изменен компонент категорий

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react"
-import CustomSelect from "./UI/CustomSelect"
-import React, { useRef } from 'react';
-import HoldButton from 'react-hold-button';  
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 
 export default function Caregories({categoriesList}){  
   
   const [currentCategory, setCurrentCategory] = useState(categoriesList[0])
-  const scrollRef = useRef(null);
 
   return (
-    <>
- 
-      <div className="categories" ref={scrollRef} >  
-        <CustomSelect
-          listOfVariants={categoriesList} 
-          currentVariant={currentCategory}
-          setVariant={setCurrentCategory}
-        />
-      </div>
-
-    </>
+      <Swiper slidesPerView={'auto'} spaceBetween={15} className="categories">
+        {
+          categoriesList.map( category => 
+            <SwiperSlide 
+              className={ currentCategory.id == category.id ? 'category active' : 'category '}
+              onClick={ () => setCurrentCategory(category) }
+              key={category.id}
+            >
+              {category.name}
+            </SwiperSlide>
+          )
+        }
+      </Swiper>
   )
 }
