df2b932b2b8e508adbb06e28a961c02bba7fcc0e
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-16 10:36:52 +0500

    исправлены стили

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,13 +1,3 @@
-import classes from './style.module.scss'
+import Search from "./Search";
 
-export default () => {
-    return (
-        <div className={classes.root}  >
-            <svg xmlns="http://www.w3.org/2000/svg" width='16px' height="16px" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                < path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-            <input placeholder="Поиск пиццы..." />
-        </div>
-    
-        )
-}
+export default Search
